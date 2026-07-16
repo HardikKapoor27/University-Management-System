5e153a1434d8b9acad551fe5757b15b1b42676f9
HARDIK KAPOOR <161691002+HardikKapoor27@users.noreply.github.com>
2026-07-16 16:48:08 +0530

    Delete ums-backend directory

--- ums-backend/src/main/java/com/university/ums/entity/Gender.java
+++
@@ -1,5 +0,0 @@
-package com.university.ums.entity;
-
-public enum Gender {
-    MALE, FEMALE, OTHER
-}
--- ums-backend/src/main/java/com/university/ums/exception/DuplicateResourceException.java
+++
@@ -1,7 +0,0 @@
-package com.university.ums.exception;
-
-public class DuplicateResourceException extends RuntimeException {
-    public DuplicateResourceException(String message) {
-        super(message);
-    }
-}
